86332b505bee6dabb975150b71b7ab698e9d2074
iMaath2 <iMaath2@gmail.com>
2025-11-18 06:07:16 -0300

    Prática 6 – Parte 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
